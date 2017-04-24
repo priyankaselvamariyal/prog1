8aab4435217e3334ee71b312021a4eb1bec79fd2
priyankaselvamariyal <priyankaselvamariyal@gmail.com>
2017-04-24 11:00:24 +0530

    Create prog1.c

---
+++ prog1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+int n;
+if(n<0)
+{
+printf("number is negative");
+}
+elseif
+{
+printf("number is positive");
+}
+else
+{
+printf("number is zero");
+}
+getch();
+}
